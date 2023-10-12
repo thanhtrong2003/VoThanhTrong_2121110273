9152b14434fa62de1903898572ce30cb0789c3ee
thanhtrong2003 <vothanhtrongmkt@gmail.com>
2023-10-12 18:07:28 +0700

    Project 10/12/2023

---
+++ AllUserControl/UC_CustomerRes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VoThanhTrong_2121110273_DoAnWindowsForms.AllUserControl
+{
+    public partial class UC_CustomerRes : UserControl
+    {
+        public UC_CustomerRes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAllotCustomer_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
